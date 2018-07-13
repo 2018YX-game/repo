f9bf33a3b0d8a6956fe656caffa86da6f9ed0d30
zhang0peter <zsd780885029@gmail.com>
2018-07-13 14:13:01 +0800

    开始界面

--- game/WINDOW/startpage.h
+++ game/WINDOW/startpage.h
@@ -16,9 +16,11 @@ public:
     ~startpage();
 
 private slots:
-    void on_pushButton_2_clicked();
 
-    void on_pushButton_clicked();
+
+    void on_startbutton_clicked();
+
+    void on_quitbutton_clicked();
 
 private:
     Ui::startpage *ui;
