ac1a8dd25b629d299e5ba8e30707dba3608af5d2
zhang0peter <zsd780885029@gmail.com>
2018-07-14 16:40:35 +0800

    调整窗口界面

--- game/WINDOW/gamewindow.cpp
+++ game/WINDOW/gamewindow.cpp
@@ -18,27 +18,28 @@ void gamewindow::on_returntostartButton_clicked()
 {
 
 }
-void gamewindow::set_Martix(std::shared_ptr<SWMatrix> spMartix){
-    this->_spMartix=spMartix;
+void gamewindow::set_Martix(std::shared_ptr<SWMatrix> spMartix) {
+    this->_spMartix = spMartix;
 }
 void gamewindow::paintEvent(QPaintEvent *)
 {
     QPainter painter(this);
-/*    painter.setPen(QPen(Qt::blue,4));//设置画笔形式
-    painter.drawLine(20,20,220,220);//画直线
-    painter.drawLine(20,220,220,20);
-    painter.drawEllipse(20,20,200,200);//画圆
-    painter.drawRect(40,40,200,200);//画矩形
-    */
-    int col= this->_spMartix->getMatrixCol();
-      int row= this->_spMartix->getMatrixRow();
-  //  int col=5;int row=5;
-       int i,j;
-       for(i=0;i<row;i++){
-           for(j=0;j<col;j++){
-               painter.setPen(Qt::black);
-               painter.setBrush(Qt::black);
-               painter.drawEllipse(40+80*j,40+80*i,40,40);
-           }
-       }
+    /*    painter.setPen(QPen(Qt::blue,4));//设置画笔形式
+        painter.drawLine(20,20,220,220);//画直线
+        painter.drawLine(20,220,220,20);
+        painter.drawEllipse(20,20,200,200);//画圆
+        painter.drawRect(40,40,200,200);//画矩形
+        */
+    int col = this->_spMartix->getMatrixCol();
+    int row = this->_spMartix->getMatrixRow();
+    //  int col=5;int row=5;
+    this->resize(100 + 80 * col, 100 + 80 * row);
+    int i, j;
+    for (i = 0; i < row; i++) {
+        for (j = 0; j < col; j++) {
+            painter.setPen(Qt::black);
+            painter.setBrush(Qt::black);
+            painter.drawEllipse(40 + 80 * j, 40 + 80 * i, 40, 40);
+        }
+    }
 }
