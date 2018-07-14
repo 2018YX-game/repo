92dd88e6bd63d361ddf6148237497e5c3ad46067
zhang0peter <zsd780885029@gmail.com>
2018-07-14 16:17:27 +0800

    游戏界面修改

--- game/WINDOW/gamewindow.cpp
+++ game/WINDOW/gamewindow.cpp
@@ -6,6 +6,7 @@ gamewindow::gamewindow(QWidget *parent) :
     ui(new Ui::gamewindow)
 {
     ui->setupUi(this);
+    set_Martix(NULL);
 }
 
 gamewindow::~gamewindow()
@@ -16,4 +17,28 @@ gamewindow::~gamewindow()
 void gamewindow::on_returntostartButton_clicked()
 {
 
+}
+void gamewindow::set_Martix(std::shared_ptr<SWMatrix> spMartix){
+    this->_spMartix=spMartix;
+}
+void gamewindow::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+/*    painter.setPen(QPen(Qt::blue,4));//设置画笔形式
+    painter.drawLine(20,20,220,220);//画直线
+    painter.drawLine(20,220,220,20);
+    painter.drawEllipse(20,20,200,200);//画圆
+    painter.drawRect(40,40,200,200);//画矩形
+    */
+    int col= this->_spMartix->getMatrixCol();
+      int row= this->_spMartix->getMatrixRow();
+  //  int col=5;int row=5;
+       int i,j;
+       for(i=0;i<row;i++){
+           for(j=0;j<col;j++){
+               painter.setPen(Qt::black);
+               painter.setBrush(Qt::black);
+               painter.drawEllipse(40+80*j,40+80*i,40,40);
+           }
+       }
 }
--- game/WINDOW/gamewindow.h
+++ game/WINDOW/gamewindow.h
@@ -1,8 +1,9 @@
 #ifndef GAMEWINDOW_H
 #define GAMEWINDOW_H
-
+#include"SWMatrix.h"
+#include"memory"
 #include <QWidget>
-
+#include<qpainter.h>
 namespace Ui {
 class gamewindow;
 }
@@ -14,12 +15,14 @@ class gamewindow : public QWidget
 public:
     explicit gamewindow(QWidget *parent = 0);
     ~gamewindow();
-
+    void set_Martix(std::shared_ptr<SWMatrix> spMartix);
+void paintEvent(QPaintEvent *);
 private slots:
     void on_returntostartButton_clicked();
 
 private:
     Ui::gamewindow *ui;
+    std::shared_ptr<SWMatrix> _spMartix;
 };
 
 #endif // GAMEWINDOW_H
