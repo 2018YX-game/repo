24ae4eca7c90c2819bdddd7c7274cd97ec3cf3d4
zhang0peter <zsd780885029@gmail.com>
2018-07-15 08:51:38 +0800

    游戏界面更改

--- game/WINDOW/gamewindow.cpp
+++ game/WINDOW/gamewindow.cpp
@@ -30,16 +30,32 @@ void gamewindow::paintEvent(QPaintEvent *)
         painter.drawEllipse(20,20,200,200);//画圆
         painter.drawRect(40,40,200,200);//画矩形
         */
-    int col = this->_spMartix->getMatrixCol();
-    int row = this->_spMartix->getMatrixRow();
-    //  int col=5;int row=5;
+  //  int col = this->_spMartix->getMatrixCol();
+  //  int row = this->_spMartix->getMatrixRow();
+      int col=5;int row=5;
     this->resize(100 + 80 * col, 100 + 80 * row);
+    painter.setBrush(Qt::blue);
+    painter.drawRect(this->rect());
     int i, j;
     for (i = 0; i < row; i++) {
         for (j = 0; j < col; j++) {
-            painter.setPen(Qt::black);
-            painter.setBrush(Qt::black);
-            painter.drawEllipse(40 + 80 * j, 40 + 80 * i, 40, 40);
-        }
+            bool color=0;
+            bool isStart;
+            if(isStart==1){
+
+            }
+            else if (color==1){
+                painter.setPen(Qt::white);
+                painter.setBrush(Qt::white);
+                painter.drawEllipse(40 + 80 * j, 40 + 80 * i, 40, 40);
+
+            }
+            else{
+                painter.setPen(Qt::black);
+                painter.setBrush(Qt::black);
+                painter.drawEllipse(40 + 80 * j, 40 + 80 * i, 40, 40);
+
+            }
+          }
     }
 }
