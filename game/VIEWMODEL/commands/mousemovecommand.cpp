61a4073f5ee95a8de84309b2cd9b5912bbebb6d7
VanneLv <505888572@qq.com>
2018-07-17 14:20:02 +0800

    鼠标command

--- game/VIEWMODEL/commands/mousemovecommand.cpp
+++ game/VIEWMODEL/commands/mousemovecommand.cpp
@@ -4,12 +4,29 @@
 mouseMoveCommand::mouseMoveCommand(SWViewModel *ptr_SWViewModel)
 {
     ptr_SWViewModel_ = ptr_SWViewModel;
+    mouseOnRow = 0;
+    mouseOnCol = 0;
 }
 
 void mouseMoveCommand::SetParameter(const _new_any_space_::any& param)
 {
-    mouseOnRow = _new_any_space_::any_cast<int>(param[0]);
-    mouseOnCol = _new_any_space_::any_cast<int>(param[1]);
+    std::string coor = _new_any_space_::any_cast<std::string>(param);
+    int i = 0;
+    while (i < coor.size()){
+        if(coor[i] >= '0' && coor[i] <= '9'){
+            if(mouseOnRow == 0)
+                mouseOnRow = coor[i] - '0' + 0;
+            else
+                mouseOnCol = coor[i] - '0' + 0;
+        }
+        else if(coor[i] == ' '){
+            ;
+        }
+        i++;
+    }
+
+    //mouseOnRow = _new_any_space_::any_cast<int>(param);
+    //mouseOnCol = _new_any_space_::any_cast<int>(param);
 }
 
 void mouseMoveCommand::Exec()
